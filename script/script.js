c17a7578b7fa4ceb000422db69d2094dc0fc338e
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2012-11-06 19:55:19 +0530

    Working with minimizing widgets

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 var normalizeEvent = function(event) {
     if(!event.stopPropagation) {
         event.stopPropagation = function() { event.cancelBubble = true; };
@@ -20,96 +21,116 @@ var addListener = function(node, type, handler, flag) {
         return;
     }
     node.attachEvent("on" + type, handler);
-}
+};
 
 var registerEvent = function(node, type, handler, flag) {
     function wrappedHandler() {
-        handler(normalizeEvent(event || window.event));   
+        handler((event || window.event));   
     }
     addListener(node, type, wrappedHandler, flag);
 };
 
-var WidgetContent = {
-	title: "Widget Title",
-	body: "Widget Body"
-}
+var Widget = (function(){
 
-var createWidgetTitleIcons = function() {
-	var icons = document.createElement("ul");
-	icons.setAttribute("class", "icons");
+	var column = 0,
+		widgetBox =  null,
+		widgetTitleBox =  null,
+		widgetBody =  null,
+		minimize =  null,
+		close =  null;
+	
+	var WidgetContent = {
+		title: "Widget Title",
+		body: "Widget Body"
+	};
 
-	var widgetSettings = document.createElement("li");
-	widgetSettings.setAttribute("class", "widget-settings");
-	widgetSettings.innerHTML = "<a></a>"
-	var minimize = document.createElement("li");
-	minimize.innerHTML = "<a class='minimize'></a>";
-	var close = document.createElement("li");
-	close.innerHTML = "<a class='close'></a>";
+	return {
+		
+		createWidgetSettingList : function() {
+			var list = document.createElement("ul");
+			list.setAttribute("class", "widget-setting-list");
+			list.innerHTML = "<li>Edit Setting</li><li>Delete this widget</li><li>Share this widget</li><li>About this wdget</li><li>You might also like</li>";
+			return list;
+		},
 
-	icons.appendChild(widgetSettings);
-	icons.appendChild(minimize);
-	icons.appendChild(close);
+		createWidgetTitleIcons : function() {
+			var icons = document.createElement("ul");
+			icons.setAttribute("class", "icons");
 
+			var widgetSettings = document.createElement("li");
+			widgetSettings.setAttribute("class", "widget-settings");
+			widgetSettings.innerHTML = "<a></a>"
+			widgetSettings.appendChild(this.createWidgetSettingList());
+			var widgetSettingList = this.createWidgetSettingList();
+			minimize = document.createElement("li");
+			minimize.setAttribute("id", "minimize");
+			minimize.setAttribute("class", "minimize");
+			close = document.createElement("li");
+			close.setAttribute("class", "close");
+			icons.appendChild(widgetSettings);
+			icons.appendChild(minimize);
+			icons.appendChild(close);
 
-	//icons.appendChild("<li><a class='minimize'></a></li>");
-	//icons.appendChild("<li><a class='close'></a></li>");
+			return icons;
+		},
 
-	return icons;
-};
+		createWidgetTitle : function(widget_title) {
+			var logo = document.createElement("div");
+			logo.setAttribute("class", "logo");
 
-var createWidgetTitle = function(widget_title) {
-	var logo = document.createElement("div");
-	logo.setAttribute("class", "logo");
+			var title = document.createElement("h4");
+			title.innerHTML = widget_title;
 
-	var title = document.createElement("h4");
-	title.innerHTML = widget_title;
+			var icons = this.createWidgetTitleIcons();
 
-	var icons = createWidgetTitleIcons();
+			widgetTitleBox = document.createElement("div");
+			widgetTitleBox.setAttribute("class","widget-title clearfix");
+			widgetTitleBox.appendChild(logo);
+			widgetTitleBox.appendChild(title);
+			widgetTitleBox.appendChild(icons);
+		},
 
-	var title_box = document.createElement("div");
-	title_box.setAttribute("class","widget-title clearfix");
-	title_box.appendChild(logo);
-	title_box.appendChild(title);
-	title_box.appendChild(icons);
+		createWidgetBody : function(widget_body) {
+			widgetBody = document.createElement("div");
+			widgetBody.setAttribute("class", "widget-content");
+			widgetBody.innerHTML = widget_body;
+		},
 
-	return title_box;
-};
+		createWidgetBox : function(WidgetContent) {
+			widgetBox = document.createElement("div");
+			widgetBox.setAttribute("class", "widget-box");
+			this.createWidgetTitle(WidgetContent.title);
+			this.createWidgetBody(WidgetContent.body);
+			widgetBox.appendChild(widgetTitleBox);
+			widgetBox.appendChild(widgetBody);
+			return widgetBox;
+		},
 
-var createWidgetBody = function(widget_body) {
-	var content = document.createElement("div");
-	content.setAttribute("class", "widget-content");
-	content.innerHTML = widget_body;
+		addEvents : function() {
+			registerEvent(minimize, "click", function(){
+				console.log(widgetBody);
+				widgetBody.style.display = "none";
+			}, false);
+		},
 
-	return content;
-};
+		createWidget : function() {
+			var widget = document.createElement("div");
+			widget.setAttribute("class", "widget-shadow");
+			widget.appendChild(this.createWidgetBox(WidgetContent));
+			return widget;
+		},
 
-var createWidgetBox = function(WidgetContent) {
-	var widgetBox = document.createElement("div");
-	widgetBox.setAttribute("class", "widget-box");
-	var widget_title = createWidgetTitle(WidgetContent.title);
-	var widget_body = createWidgetBody(WidgetContent.body);
-	widgetBox.appendChild(widget_title);
-	widgetBox.appendChild(widget_body);
-	return widgetBox;
-};
-
-var createWidget = function() {
-	var widget = document.createElement("div");
-	widget.setAttribute("class", "widget-shadow");
-	widget.appendChild(createWidgetBox(WidgetContent));
-	console.log(widget);
-
-	return widget;
-};
-
-var addWidget = function() {
-	var widget = createWidget();
-	var column = document.getElementById("column1");
-	console.log(column);
-	column.appendChild(widget);
-};
+		addWidget : function() {
+			var col = "column" + ((column++)%3+1);
+			document.getElementById(col).appendChild(this.createWidget());
+			this.addEvents();
+		}
+	};
+})();
 
 window.onload = function() {
 	var addButton = document.getElementById("add_widget");
-	registerEvent(addButton, "click", addWidget, false);
+	registerEvent(addButton, "click", function(){
+		Widget.addWidget();
+	}, false);
 }
