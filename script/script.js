1abdcfa9b39ea3cbb3d77793eb37b70f3d86433b
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2012-11-07 21:51:03 +0530

    Widgets can be added. Implementing overlay

--- script/script.js
+++ script/script.js
@@ -24,49 +24,49 @@ var addListener = function(node, type, handler, flag) {
 };
 
 var registerEvent = function(node, type, handler, flag) {
-    function wrappedHandler() {
-        handler((event || window.event));   
+    var wrappedHandler = function(event) {
+		handler(normalizeEvent(event || window.event));   
     }
     addListener(node, type, wrappedHandler, flag);
 };
 
+var column = 0;
+
 var Widget = (function(){
 
-	var column = 0,
+	var	widget = null,
 		widgetBox =  null,
 		widgetTitleBox =  null,
 		widgetBody =  null,
-		minimize =  null,
-		close =  null;
+		minimize = null,
+		close = null,
+		
+		WidgetContent = {
+			title: "Widget Title",
+			body: "Widget Body"
+		},
 	
-	var WidgetContent = {
-		title: "Widget Title",
-		body: "Widget Body"
-	};
+		createElement = function(tag, className) {
+			var element = document.createElement(tag);
+			element.className = className;
+			return element;
+		};
 
 	return {
 		
 		createWidgetSettingList : function() {
-			var list = document.createElement("ul");
-			list.setAttribute("class", "widget-setting-list");
+			var list = createElement("ul", "widget-setting-list");
 			list.innerHTML = "<li>Edit Setting</li><li>Delete this widget</li><li>Share this widget</li><li>About this wdget</li><li>You might also like</li>";
 			return list;
 		},
 
 		createWidgetTitleIcons : function() {
-			var icons = document.createElement("ul");
-			icons.setAttribute("class", "icons");
-
-			var widgetSettings = document.createElement("li");
-			widgetSettings.setAttribute("class", "widget-settings");
+			var icons = createElement("ul", "icons");
+			var widgetSettings = createElement("li", "widget-settings");
 			widgetSettings.innerHTML = "<a></a>"
 			widgetSettings.appendChild(this.createWidgetSettingList());
-			var widgetSettingList = this.createWidgetSettingList();
-			minimize = document.createElement("li");
-			minimize.setAttribute("id", "minimize");
-			minimize.setAttribute("class", "minimize");
-			close = document.createElement("li");
-			close.setAttribute("class", "close");
+			minimize = createElement("li", "minimize");
+			close = createElement("li", "close");
 			icons.appendChild(widgetSettings);
 			icons.appendChild(minimize);
 			icons.appendChild(close);
@@ -75,30 +75,24 @@ var Widget = (function(){
 		},
 
 		createWidgetTitle : function(widget_title) {
-			var logo = document.createElement("div");
-			logo.setAttribute("class", "logo");
-
+			var logo = createElement("div", "logo");
+			
 			var title = document.createElement("h4");
 			title.innerHTML = widget_title;
 
-			var icons = this.createWidgetTitleIcons();
-
-			widgetTitleBox = document.createElement("div");
-			widgetTitleBox.setAttribute("class","widget-title clearfix");
+			widgetTitleBox = createElement("div", "widget-title clearfix");
 			widgetTitleBox.appendChild(logo);
 			widgetTitleBox.appendChild(title);
-			widgetTitleBox.appendChild(icons);
+			widgetTitleBox.appendChild(this.createWidgetTitleIcons());
 		},
 
 		createWidgetBody : function(widget_body) {
-			widgetBody = document.createElement("div");
-			widgetBody.setAttribute("class", "widget-content");
+			widgetBody = createElement("div", "widget-content");
 			widgetBody.innerHTML = widget_body;
 		},
 
 		createWidgetBox : function(WidgetContent) {
-			widgetBox = document.createElement("div");
-			widgetBox.setAttribute("class", "widget-box");
+			widgetBox = createElement("div", "widget-box");
 			this.createWidgetTitle(WidgetContent.title);
 			this.createWidgetBody(WidgetContent.body);
 			widgetBox.appendChild(widgetTitleBox);
@@ -108,14 +102,23 @@ var Widget = (function(){
 
 		addEvents : function() {
 			registerEvent(minimize, "click", function(){
-				console.log(widgetBody);
-				widgetBody.style.display = "none";
+				var className = minimize.attributes["class"].value;
+				if(className == "minimize") {
+					widgetBody.style.display = "none";
+					minimize.className = "maximize";
+				}
+				else {
+					widgetBody.style.display = "block";
+					minimize.className = "minimize";
+				}
+			}, false);
+			registerEvent(close, "click", function(){
+				widget.parentNode.removeChild(widget);
 			}, false);
 		},
 
 		createWidget : function() {
-			var widget = document.createElement("div");
-			widget.setAttribute("class", "widget-shadow");
+			widget = createElement("div", "widget-shadow");
 			widget.appendChild(this.createWidgetBox(WidgetContent));
 			return widget;
 		},
@@ -126,11 +129,13 @@ var Widget = (function(){
 			this.addEvents();
 		}
 	};
-})();
+});
 
 window.onload = function() {
 	var addButton = document.getElementById("add_widget");
 	registerEvent(addButton, "click", function(){
-		Widget.addWidget();
+		var w = new Widget();
+		w.addWidget();
+		//Widget.addWidget();
 	}, false);
 }
