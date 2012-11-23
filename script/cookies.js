4ee4ed1befc7c2c559dd1dfb59e75497c20e85f1
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2012-11-23 10:24:02 +0530

    code refactoring

--- script/cookies.js
+++ script/cookies.js
@@ -18,10 +18,8 @@ var Cookies = (function() {
 			var cookies = document.cookie.split(";");
 			for(i=0, len=cookies.length; i<len; i++) {
 				var cookie = cookies[i];
-				while(cookie.charAt(0) == " "){ 
-					cookie = cookie.substring(1, cookie.length);
-				}
-				if(cookie.indexOf(name) == 0) {
+				cookie = cookie.replace(/^\s+|\s+$/g,'');
+				if(cookie.indexOf(name) === 0) {
 					return cookie.substring(name.length, cookie.length);
 				}
 			}
